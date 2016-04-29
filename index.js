8aacde7a16a66003bbcac61cd776efe69959ece0
Barbara Maim <barbara@minsh.net>
2016-04-29 15:57:41 +0530

    eyeCatch img

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 var assign = require('object-assign');
 
 hexo.config.amp_generator = assign({
-	
+
 }, hexo.config.amp_generator);
 
 hexo.extend.generator.register('amp', require('./lib/generator'));
 hexo.extend.filter.register('after_post_render', require('./lib/eyeCatchVars') );
-
